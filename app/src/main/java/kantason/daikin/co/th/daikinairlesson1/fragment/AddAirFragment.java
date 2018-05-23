0ca0cd2db6239424bc1a4f09b0af27aabe4ea1c7
test <test@hotmail.com>
2018-05-23 13:09:32 +0700

    toolbar of Addair finish

--- app/src/main/java/kantason/daikin/co/th/daikinairlesson1/fragment/AddAirFragment.java
+++ app/src/main/java/kantason/daikin/co/th/daikinairlesson1/fragment/AddAirFragment.java
@@ -4,13 +4,40 @@ package kantason.daikin.co.th.daikinairlesson1.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import kantason.daikin.co.th.daikinairlesson1.MainActivity;
 import kantason.daikin.co.th.daikinairlesson1.R;
 
 public class AddAirFragment extends Fragment {
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //create tool bar
+        createToolBar();
+    }  //  Main metoho
+
+    private void createToolBar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarAddAir);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Add Air Condition");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill all Blank");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
     @Nullable
     @Override
